e04becdf736879a08bb8f906a2298b88b1437840
shann03 <shannjongaya03@gmail.com>
2026-02-17 13:32:28 +0800

    Add AdminSetup route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AdminProvider } from './contexts/AdminContext';
 import { UserProvider } from './contexts/UserContext';
 import AdminLogin from './pages/AdminLogin';
+import AdminSetup from './pages/AdminSetup';
 import AdminDashboard from './pages/AdminDashboard';
 import PublicHome from './pages/PublicHome';
 import Register from './pages/Register';
@@ -17,6 +18,7 @@ function App() {
           <Routes>
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/admin/setup" element={<AdminSetup />} />
             <Route path="/admin/login" element={<AdminLogin />} />
             <Route path="/admin/dashboard" element={<AdminDashboard />} />
             <Route
